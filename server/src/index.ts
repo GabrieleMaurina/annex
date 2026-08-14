2d87ae1738b16685c173f4bc23e5131ed9fa8695
GabrieleMaurina <gabrielemaurina95@gmail.com>
2026-08-13 21:47:38 -0600

    feat/added-server-files

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+  console.log('client connected', socket.id);
+});
+
+const port = 3000;
+httpServer.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
